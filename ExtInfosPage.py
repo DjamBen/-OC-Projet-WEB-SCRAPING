1ac6d604d811030548d3e4f30c7d260883cb110c
Djam <mrbenboureche@hotmail.com>
2021-04-21 12:47:03 +0200

    Version finale du script pour une page

--- ExtInfosPage.py
+++ ExtInfosPage.py
@@ -1,7 +1,8 @@
 import requests as req
 from bs4 import BeautifulSoup
 import csv
-
+import os
+import re
 
 
 def Infos_Page(url):
@@ -13,25 +14,38 @@ def Infos_Page(url):
         desc = ""
     else:
         desc = desc.find_next('p').get_text()
-        catego = soup.find("ul",{"class": "breadcrumb"}).find_next("li").find_next("li").find_next("li").get_text()
+        catego = soup.find("ul",{"class": "breadcrumb"}).find_next("li").find_next("li").find_next("li").get_text().strip('\n')
         infos_table = soup.table
         infos = {}
         for row in infos_table.find_all("tr"):
             infos[row.th.get_text()] = row.td.get_text()
+        Titre = soup.find('h1').text
+        Image = soup.img["src"].strip('../../')
+        Image_book = 'http://books.toscrape.com/' + Image
         
-    info_books = { 
-        "Titre": soup.title.text,
-        "image": soup.img["src"],
+    
+    infos_book = { 
+        "Titre": Titre,
+        "image": Image_book,
         "Description": desc,
         "Categorie": catego,
         **infos
         }
     
-    print(info_books)
+    path = "Image pour une page"
+    
+    if not os.path.exists(path):
+        os.makedirs(path)
+        
+        title = re.sub('[^a-zA-Z0-9 \n]', '', Titre)
+        
+        with open(path + '/' + title + ".jpg", "wb") as file:
+            res = req.get(Image_book)
+            file.write(res.content)
+    
+    print(infos_book)
     
     
     
-
-
-
+    
 Infos_Page("http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html")
